8b9f3adc482a18997c005f32c3cd9da04263b06d
MaxShutov <razonmaks@gmail.com>
2019-03-06 23:35:19 +0000

    Unpacked zip of the game with platforms

---
+++ Game2D v.0.2/Assets/Scripts/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+	private Transform player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player").transform;
+	}
+
+	void LateUpdate(){
+		transform.position = new Vector3 (player.position.x, player.position.y+1, -10);
+	}
+
+
+}
