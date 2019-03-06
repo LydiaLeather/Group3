2d8aa71a425a23f3f649ed9f2698dc51ecaabeb7
MaxShutov <razonmaks@gmail.com>
2019-03-06 22:55:15 +0000

    Unpacked a zip folder with the game

---
+++ Game2D/Assets/Scripts/EagleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EagleController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate(8f*Time.deltaTime,0,0);
+		if (gameObject.transform.position.x >11) {
+			Destroy(transform.parent.gameObject);
+		}
+	}
+}
---
+++ Game2D/Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour {
+
+	public static ScoreCounter scoreCounter;
+	public Text scoreText;
+	int score = 0;
+
+	public void IncreaseScore(int s){
+		score += s;
+		scoreText.text = score + "";
+	}
+
+	// Use this for initialization
+	void Start () {
+		scoreCounter = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
