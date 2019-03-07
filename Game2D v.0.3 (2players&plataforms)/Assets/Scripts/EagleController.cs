f45a96a3881e7e891a15930dc4da3f953400ebdf
MaxShutov <razonmaks@gmail.com>
2019-03-07 12:42:48 +0000

    Unpacked the archive with the new version of the game

---
+++ Game2D v.0.3 (2players&plataforms)/Assets/Scripts/EagleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EagleController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate(8f*Time.deltaTime,0,0);
+		if (gameObject.transform.position.x >11) {
+			Destroy(transform.parent.gameObject);
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision){
+		//gameObject.SetActive(false);
+		Destroy(transform.parent.gameObject);
+	}
+}
---
+++ Game2D v.0.3 (2players&plataforms)/Assets/Scripts/Fireball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour {
+
+	private float speed = 8;
+	Rigidbody2D rb;
+	private Vector2 dir;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.velocity = dir * speed;
+	}
+
+	public void setDirection(Vector2 dir){
+		this.dir = dir;
+	}
+
+	//public void addxpos(float x){}
+
+	void OnBecameInvisible(){
+		Destroy (gameObject);
+	}
+}
