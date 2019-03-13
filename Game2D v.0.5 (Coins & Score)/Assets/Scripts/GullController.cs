5dfe08b72cb40d3ba37cf609c5b041457d561e6a
MaxShutov <razonmaks@gmail.com>
2019-03-13 22:45:14 +0000

    Implemented coins, score and reasonable spawning of platforms

--- Game2D v.0.5 (Coins & Score)/Assets/Scripts/GullController.cs
+++ Game2D v.0.5 (Coins & Score)/Assets/Scripts/GullController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class GullController : MonoBehaviour {
 
 	// transform variables to access both players
