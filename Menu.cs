a0fd0d3bdeb240df4e2edea42cf168ca6d483e80
MachineLearni <44461463+MachineLearni@users.noreply.github.com>
2019-03-28 02:47:07 +0530

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    //private void OnGUI()
+    //{
+    //if(GUI.Button(new Rect(Screen.width/2.5f,Screen.height/3,Screen.width/5,Screen.height/10), "Load Game"))
+    // {
+   // Application.LoadLevel(1);
+        //}
+        //if (GUI.Button(new Rect(Screen.width / 2.5f, Screen.height / 2, Screen.width / 5, Screen.height / 10), "Exit Game"))
+        //{
+            //Application.Quit();
+        //}
+
+        //}
+    //}
+     void start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+    public void StartGame()
+    {
+        Application.LoadLevel(0);
+    }
+    public void Ouit()
+    {
+        Application.Quit();
+    }
+}
